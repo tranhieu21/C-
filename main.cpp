d3fb783d668140071979431842f346cf6f7c2e11
tranhieu21 <114563520+tranhieu21@users.noreply.github.com>
2022-12-19 18:10:56 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <deque>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    deque <int> A;
+    deque <int>::const_iterator cIter;
+
+//push добавления элемента в конец
+    A.push_back(1);
+    A.push_back(2);
+    A.push_back(3);
+    
+    cout << "A = ";
+    for (int i : A)
+        cout << i << ", ";
+    cout << endl;
+    
+//pop удаления элемента из начала
+    A.pop_front();
+    
+    cout << "A - удаления элемента из начала: ";
+    for (int i : A)
+        cout << i << ", ";
+    cout << endl;
+
+//front получения первого элемента
+    int& a = A.front();
+    cout << "первый элемент A: " << a;
+    cout << endl;
+
+//back получения последнего элемента
+    int& z = A.back();
+    cout << "последный элемент A: " << z;
+    cout << endl;
+    
+//size получения размера очереди
+    cout << "размера A: " << A.size();
+    cout << endl;
+
+}
